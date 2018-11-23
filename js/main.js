d4dcca37e55b00909524dce0a6a2aa1a14e4b385
Philipp Kats <casyfill@gmail.com>
2018-11-23 17:51:38 -0500

    single js

--- js/main.js
+++ js/main.js
@@ -1,103 +1,76 @@
-
+// console.log(CITY);
 var VIEW;
-
-let colors = {
-  "primary":['#032765',          // dark-blue
-             '#CEF4FF',          // light-blue
-             '#0195E5'],         // bright-blue
-  "secondary":['#ABD6CC',        // willow
-               '#75AAA5',        // snapdragon
-               '#387D7D',        // anemone
-               '#00AAE5',        // cerulean
-               '#00669E',        // denim
-               '#004984'         // sapphire
-              ],
-  "inactive":['#CDCDCD',         // CELL GREY
-              '#F5F5F5',         // BACKGROUND GREY
-  ],
-  "accent":['#3FE8DF',           // BRIGHT TEAL
-            '#E0E527',           // OLIVE
-            '#7329FF'            // BRIGHT PURPLE
-]
-};
-
-
 vega.scheme('se_cat', ['#032765', '#0195E5', '#387D7D',
-                       '#00AAE5', '#3FE8DF', '#E0E527', '#7329FF']);
+  '#00AAE5', '#3FE8DF', '#E0E527', '#7329FF'
+]);
 
 vega.scheme('se_cat_binary', ['#0195E5', '#032765']);
 vega.scheme('se_cat_accent', ['#032765', '#3FE8DF', '#E0E527', '#7329FF']);
 
-// API data call
-function getParameterByName(name, url) {
-     if (!url) url = window.location.href;
-     name = name.replace(/[\[\]]/g, "\\$&");
-     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-     if (!results) return 404;
-     if (!results[2]) return 404; // ???
-     return decodeURIComponent(results[2].replace(/\+/g, " ")).replace(/\"/g, ""); //Remove qoutes
-    }
 
-// RETURN datafile and template
-function getVisParams(){
-    // let datafile= 'data/' + getParameterByName('data')
-    return {"data": 'specs.vg.json'};
+// RETURN specs
+function getVisParams() {
+  let specsfile = 'specs/' + CITY + '.vg.json';
+  return {
+    "data": specsfile
+  };
 }
 
-function onError(error){
+function onError(error) {
   console.log(error);
   var window = d3.select('#window');
   window.selectAll("*").remove();
   window.append("img")
-        .attr("src", "assets/404.jpg")
-        .attr("id", "v404")
-        .attr("alt", "something went wrong...");
+    .attr("src", "assets/404.jpg")
+    .attr("id", "v404")
+    .attr("alt", "something went wrong...");
 }
 
-let settings = getVisParams();  // get app settings
+let settings = getVisParams(); // get app settings
 var view;
 var loader = vega.loader();
 loader.load(settings['data'])
-  .then(function(data) { render(JSON.parse(data)); })
-  .catch(function(error) {
-          onError(error)
-});
+  .then(function (data) {
+    render(JSON.parse(data));
+  })
+  .catch(function (error) {
+    onError(error)
+  });
 
 
-function set_custom(spec){
+function set_custom(spec) {
   // set title and subtitle from usermeta part of specs
-  if (spec['usermeta']['title']){
+  if (spec['usermeta']['title']) {
     d3.select('h1.Title').text(spec['usermeta']['title']);
   }
 
-  if (spec['usermeta']['subtitle']){
+  if (spec['usermeta']['subtitle']) {
     d3.select('h2.Secondary').text(spec['usermeta']['subtitle']);
   }
 
 
-  if (spec['usermeta']['footnotes']){
+  if (spec['usermeta']['footnotes']) {
     var notes = spec['usermeta']['footnotes'],
-        notes_text = '';
+      notes_text = '';
 
-    for (i = 0; i < notes.length; i++) { 
-    notes_text += '<p>' + notes[i] + "</p>";
-}
+    for (i = 0; i < notes.length; i++) {
+      notes_text += '<p>' + notes[i] + "</p>";
+    }
     d3.select('div#footnotes').html(notes_text);
   }
 }
 
 
-function prepare_tooltip_opts(spec){
-      
-    for (i = 0; i < spec['fields'].length; i++) { 
-      if(spec['fields'][i]['render']){
-        var f = spec['fields'][i]['render'];
-        console.log(f)
-        spec['fields'][i]['render'] = new Function(f.arguments, f.body);
-      }
-    
+function prepare_tooltip_opts(spec) {
+
+  for (i = 0; i < spec['fields'].length; i++) {
+    if (spec['fields'][i]['render']) {
+      var f = spec['fields'][i]['render'];
+      console.log(f)
+      spec['fields'][i]['render'] = new Function(f.arguments, f.body);
     }
+
+  }
 
   console.log(spec);
   return spec
@@ -106,37 +79,41 @@ function prepare_tooltip_opts(spec){
 
 function render(vlSpec) {
 
-    set_custom(vlSpec);
-    let opt = {"actions":false, "mode":"vega-lite", "renderer": 'svg'};
+  set_custom(vlSpec);
+  let opt = {
+    "actions": false,
+    "mode": "vega-lite",
+    "renderer": 'svg'
+  };
 
-    if (!vlSpec["$schema"].includes("vega-lite") ) {
-      opt.mode = 'vega';
-    }
+  if (!vlSpec["$schema"].includes("vega-lite")) {
+    opt.mode = 'vega';
+  }
 
-    vegaEmbed('#Container-Data', vlSpec, opt)
-        .then(function(result) {
-          // result.view is the Vega View, vlSpec is the original Vega-Lite specification
-          VIEW = result.view;
-          let w = $('div#Container-Data').width();
-          let h = $('div#Container-Data').height();
-          console.log(w, h);
-          VIEW.width(w).height(h).run();
-          // VIEW.logLevel(vega.Debug);
-          vegaTooltip.default(VIEW);
-        })
-        .catch(console.error);
+  vegaEmbed('#Container-Data', vlSpec, opt)
+    .then(function (result) {
+      // result.view is the Vega View, vlSpec is the original Vega-Lite specification
+      VIEW = result.view;
+      let w = $('div#Container-Data').width();
+      let h = $('div#Container-Data').height();
+      console.log(w, h);
+      VIEW.width(w).height(h).run();
+      // VIEW.logLevel(vega.Debug);
+      vegaTooltip.default(VIEW);
+    })
+    .catch(console.error);
 }
 
 
-$(window).on('resize', function() {
-//     // assume I have an element and viewInstance variables representing
-//     // the container, and vega View instance, respectively.
-    // let element = $('div#Container-Data')
-    let w = window.innerWidth;
-    let h = window.innerHeight - 50;
-    console.log('window size:', w, h);
+$(window).on('resize', function () {
+  //     // assume I have an element and viewInstance variables representing
+  //     // the container, and vega View instance, respectively.
+  // let element = $('div#Container-Data')
+  let w = window.innerWidth;
+  let h = window.innerHeight - 50;
+  console.log('window size:', w, h);
 
-    VIEW.width(w).height(h).run();
-    // console.log(VIEW.getState());
+  VIEW.width(w).height(h).run();
+  // console.log(VIEW.getState());
 
 });
--- makefile
+++ makefile
@@ -4,3 +4,8 @@ server2:
 server:
 		python -m http.server 8000
 
+# dryrun:
+# 	aws s3 sync . s3://streeteasy-research/data_download_prototype/charts  --dryrun --exclude ".git/*" --exclude ".DS_Store" --exclude ".gitignore" --exclude "scripts/*"  --exclude ".py"
+
+# upload:
+# 	aws s3 sync . s3://streeteasy-research/data_download_prototype/charts --exclude ".git/*" --exclude ".DS_Store" --exclude ".gitignore" --exclude "scripts/*"  --exclude ".py"
